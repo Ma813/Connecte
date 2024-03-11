09c552747beb3a1bc65b4ae3ae95bf1ec02a723e
Matas Gudliauskas <matgud@ktu.lt>
2024-03-11 19:44:53 +0200

    Modified About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
 export default function About() {
     return (
         <div>
-            <h1>About page</h1>
+            <h1>Connectė</h1>
+                <p>Connectė is an implementation of the game Connect4 using React and Python flask. In the game you can play against other opponents or a bot that utilizes the alpha-beta pruning algorithm to select the moves that lead to the best outcome. The website also implements multiple game modes that would be impossible to play on a physical board.</p>
+                <h2>Authors:</h2>
+                <p>Martynas Mačiulaitis</p>
+                <p>Martynas Jukna</p>
+                <p>Dominykas Pranaitis</p>
+                <p>Ainis Vaičiūnas</p>
+                <p>Julius Barauskas</p>
+                <p>Matas Gudliauskas</p>
         </div>
     )
 }
