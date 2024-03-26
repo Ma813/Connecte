3e6cb8cf6e6978003959132ea35f4bec78e76164
Svies4 <118697861+Svies4@users.noreply.github.com>
2024-03-26 21:54:16 +0200

    fixed routing issue

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import About from "./pages/About";
 import CreateRoom from "./pages/CreateRoom";
 import Room from "./pages/Room";
+import Profile from "./pages/Profile"; // Assuming you want to include this as well
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const routes = [
   { path: "/", element: <CreateRoom /> }, // Updated path for About page
   { path: "/about", element: <About /> }, // Updated path for CreateRoom page
-  { path: "/room/:gameId", element: <Room /> } // Updated path for Room page
+  { path: "/room/:gameId", element: <Room /> }, // Updated path for Room page
+  { path: "/profile", element: <Profile /> }, // Adding Profile to the routes
+  { path: "/register", element: <RegisterPage /> }, // Adding RegisterPage to the routes
+  { path: "/login", element: <LoginPage /> } // Adding LoginPage to the routes
 ];
 
 export default routes;
