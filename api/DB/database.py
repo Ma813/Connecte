66d7f355927f1f1d4f7267cc4b88018b1baf8a65
GunterDa <domas.pranaitis@gmail.com>
2024-03-26 15:03:37 +0200

    Added password hashing, salting and peppering

--- api/DB/database.py
+++ api/DB/database.py
@@ -1,24 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
-#from WebSocket.webSocket import app
+# from WebSocket.webSocket import app
 from flask import Flask, Response, request, Blueprint
 from extensions import da
+import json
+import bcrypt
+import hmac
+import hashlib
 
 import datetime
 
 datab = Blueprint(name="datab", import_name=__name__)
+pepper = json.load(open("config.json"))
+
 
 class PLAYERS(da.Model):
     username = da.Column(da.String(50), primary_key=True)
     hashed_pass = da.Column(da.String(255))
     email = da.Column(da.String(255))
 
+
 class GAMES(da.Model):
-    id=da.Column(da.Integer, primary_key=True, autoincrement=True)
+    id = da.Column(da.Integer, primary_key=True, autoincrement=True)
     game_board = da.Column(da.String(1024))
-    time_date= da.Column(da.DateTime, default=datetime.datetime.now())
+    time_date = da.Column(da.DateTime, default=datetime.datetime.now())
+
 
 class PLAYERS_GAMES(da.Model):
-    id=da.Column(da.Integer, primary_key=True, autoincrement=True)
+    id = da.Column(da.Integer, primary_key=True, autoincrement=True)
     FKplayer = da.Column(da.String(50), da.ForeignKey('players.username'))
     FKgame = da.Column(da.Integer, da.ForeignKey('games.id'))
     WDL = da.Column(da.String(1))
@@ -29,20 +37,24 @@ class PLAYERS_GAMES(da.Model):
 def createPlayer():
     try:
         data = request.get_json()
-        usern=data['username']
-        passw=data['hashed_pass']
-        mail=data['email']
+        usern = data['username']
+        passw = data['hashed_pass']
+        mail = data['email']
 
         if len(usern) < 5:
             return {'message': 'Username must be at least 5 characters long'}
         if len(passw) < 8:
             return {'message': 'Password must be at least 8 characters long'}
-        
-        #Validate if it is a valid email later
+
+        # Validate if it is a valid email later
 
         taken = PLAYERS.query.filter_by(username=usern).first()
         if (taken):
             return {'message': 'Username already taken'}
+
+        passw = hmac.new(pepper["Pepper"].encode('utf-8'), passw.encode('utf-8'), hashlib.sha256).hexdigest()
+        salt = bcrypt.gensalt()
+        passw = bcrypt.hashpw(passw.encode('utf-8'), salt)
 
         pl = PLAYERS(username=usern, hashed_pass=passw, email=mail)
         da.session.add(pl)
@@ -51,13 +63,15 @@ def createPlayer():
         return {'message': f'Added {usern} to database'}
     except Exception as e:
         return {'message': str(e)}
-    
+
+
 def createGuestPlayer(id):
     pl = PLAYERS(username=id, hashed_pass="", email="")
     da.session.add(pl)
     da.session.commit()
 
     return {'message': f'Added {id} to database'}
+
 
 def registerGame(gameBoard, players, winner):
     print("called")
@@ -77,16 +91,17 @@ def registerGame(gameBoard, players, winner):
             da.session.add(pg)
             da.session.commit()
 
-        return {'message': f'Added game to database', 'gameId': game.id }
+        return {'message': f'Added game to database', 'gameId': game.id}
     except Exception as e:
         return {'message': str(e)}
-    
+
 
 def userExists(usern):
     user = PLAYERS.query.filter_by(username=usern).first()
 
     return user
-    
+
+
 @datab.route('/checkUser', methods=['POST'])
 def checkUser():
     try:
@@ -97,13 +112,13 @@ def checkUser():
         user = PLAYERS.query.filter_by(username=usern).first()
         if not user:
             return {'message': 'User not found'}
-        if user.hashed_pass != passw:
+        if not bcrypt.checkpw(hmac.new(pepper["Pepper"].encode('utf-8'), passw.encode('utf-8'), hashlib.sha256).hexdigest().encode('utf-8'), user.hashed_pass):
             return {'message': 'Incorrect password'}
-
         return {'message': 'Success'}
     except Exception as e:
         return {'message': str(e)}
-    
+
+
 @datab.route('/addPlayerGame', methods=['GET'])
 def addPlayerGame():
     try:
