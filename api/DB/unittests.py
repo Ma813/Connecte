a32c810e296f6cff6a93a5002763c677c8f847d9
Matas Gudliauskas <matas@Matass-MBP.lan>
2024-04-09 19:39:09 +0300

    Fixed mock db tests

--- api/DB/unittests.py
+++ api/DB/unittests.py
@@ -7,10 +7,13 @@ class TestCreatePlayer(unittest.TestCase):
         self.request_mock = MagicMock()
         self.sql_functions_mock = MagicMock()
 
+
         import database
         self.module = database
         self.module.request = self.request_mock
         self.module.sql_functions = self.sql_functions_mock
+        
+        self.module.sql_functions.select.return_value = None
 
     def test_create_player_valid_data(self):
         self.request_mock.get_json.return_value = {
@@ -18,7 +21,7 @@ class TestCreatePlayer(unittest.TestCase):
             'hashed_pass': 'random_pass',
             'email': 'random@email.com'
         }
-        self.sql_functions_mock.select.return_value.first.return_value = None
+        # self.sql_functions_mock.select.return_value.first.return_value = None
         response = self.module.createPlayer()
         self.assertEqual(response, {'message': 'Added to database'})
 
@@ -28,7 +31,7 @@ class TestCreatePlayer(unittest.TestCase):
             'hashed_pass': 'random_pass',
             'email': 'random@email.com'
         }
-        self.sql_functions_mock.select.return_value.first.return_value = None
+        # self.sql_functions_mock.select.return_value.first.return_value = None
         response = self.module.createPlayer()
         self.assertEqual(response, {'message': 'Added to database'})
 
@@ -56,6 +59,7 @@ class TestCreatePlayer(unittest.TestCase):
             'hashed_pass': 'random_pass',
             'email': 'random@email.com'
         }
+        self.module.sql_functions.select.return_value = {'username': 'random_user'}
         response = self.module.createPlayer()
         self.assertEqual(response, {'message': 'Username already taken'})
 
@@ -76,6 +80,15 @@ class TestCreatePlayer(unittest.TestCase):
         }
         response = self.module.createPlayer()
         self.assertEqual(response, {'message': 'Added to database'})
+        
+    def test_create_player_wrong_json_format(self):
+        self.request_mock.get_json.return_value = {
+            'username': 'username',
+            'pass': 'password!@#$%^&*()_+',
+            'email': 'this@email.com'
+        }
+        response = self.module.createPlayer()
+        self.assertEqual(response, {'message': "'hashed_pass'"})
 
 if __name__ == '__main__':
     unittest.main()
