9273247fe5b2bde9a4693faafd0553b2d59e94e0
mirazas258 <mirazas258@gmail.com>
2024-05-15 20:45:49 +0300

    Made CreateRoom page a little bit nice

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -13,11 +13,12 @@ const GameRoom = () => {
     const [gameMode, setGameMode] = useState(1);
     const [winCondition, setWinCondition] = useState(4);
     const [playerCondition, setPlayerCondition] = useState(2);
-    
+
 
     const [errorMessage, setErrorMessage] = useState('');
     const [botDifficulty, setBotDifficulty] = useState(1);
     const [Loading, setLoading] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
 
 
 
@@ -30,7 +31,14 @@ const GameRoom = () => {
     }, [roomId]);
 
     const handleCreateRoomClick = () => {
-        createRoom();
+        if(heigth >= 3 && heigth <= 9 & width >= 3 && width <= 9 && winCondition > 1){
+            createRoom();
+        }
+        else
+        setErrorMessage('Height and width must be between 3 and 9 and number of circles should be at least 2.');
+        return;
+            
+        
     };
 
     const handleSubmit = () => {
@@ -39,49 +47,20 @@ const GameRoom = () => {
 
     const handleHeightChange = (e) => {
         const newHeight = parseInt(e.target.value, 10);
-        if (newHeight >= 3 && newHeight <= 15) {
+        
             setHeigth(newHeight);
             setErrorMessage(''); // Clear any previous error message
-        } else {
-            setErrorMessage('Height must be between 3 and 15.'); // Display error message
-        }
+        
     };
     const handleWidthChange = (e) => {
         const newWidth = parseInt(e.target.value, 10);
-        if (newWidth >= 3 && newWidth <= 15) {
+        
             setWidth(newWidth);
             setErrorMessage(''); // Clear any previous error message
-        } else {
-            setErrorMessage('Width must be between 3 and 15.'); // Display error message
-        }
+        
+        
     };
 
-    /* CHATBOTAS SAKE KAZKA TOKIO REIKE IDETI
-const register = async () => {
-    const userData = {
-      mode: gameMode,
-      winCondition: winCondition  // Sending win condition to the server
-    };
-
-    try {
-      const response = await fetch(path + '/getRoom', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(userData)
-      });
-
-      const responseData = await response.json();
-      if(responseData.error) throw new Error(responseData.error);
-      setRoomId(responseData.gameId);
-      navigate(`/room/${responseData.gameId}`);
-    } catch (error) {
-      console.error('Error creating/joining room:', error);
-      // Handle error state appropriately
-    }
-};
-    */
     const createRoom = async () => {
         setLoading(true);
         const userData = {
@@ -122,112 +101,134 @@ const register = async () => {
 
     const handleChange = (event) => {
         document.getElementById('roomIdInput').placeholder = 'Input Room ID'
-        document.getElementById('roomIdInput').className = 'roomUIelement'
+        document.getElementById('roomIdInput').className = 'textInput'
         setInputValue(event.target.value);
     };
-    
+
     const renderLoading = () => (
         <div>
-          <p class = "p1">Loading!</p>
-    
+            <p class="p1">Loading!</p>
+
         </div>
-      );
-    if(Loading){
+    );
+    if (Loading) {
         return renderLoading
     }
-    else{
-    return (
-        <div>
-            <div><button className='roomUIelement' onClick={handleCreateRoomClick}>Create Game Room</button></div>
-            <form onSubmit={handleSubmit}>
-                <div className='select'>
-                    <input onChange={() => setGameMode(1)} type="radio" id="standardGameCheck" name="gameMode" value="standardGame" checked={gameMode === 1}></input>
+    else {
+        return (
+            <div className='CreateRoomPage'>
+                <div><button className='button-6' onClick={handleCreateRoomClick}>Create Game Room</button></div>
+                <form onSubmit={handleSubmit} action="/action_page.php">
+                    <label htmlFor="gameMode">Please select Game mode:</label>
+                    <div className='select'>
+                        <input onChange={() => setGameMode(1)} type="radio" id="standardGameCheck" name="gameMode" value="standardGame" checked={gameMode === 1}></input>
+                        <label className="hoverLabel" htmlFor="standardGameCheck">
+                            Standart game
+                        </label>
+                        <div className="hover-text"> Standart game of Connect4 where you may choose between 2 and 4 players to play against </div>
 
-                    <label htmlFor="standardGameCheck"> Standard game</label>
-                </div>
-                <div className='select'>
-                    <label htmlFor="winConditionInput">Number of players:</label>
-                    <input
-                        id="playerInput"
-                        type="number"
-                        value={playerCondition}
-                        onChange={e => setPlayerCondition(parseInt(e.target.value, 10))}
-                        min="2"  // Minimum of 3 to make it reasonable
-                        max="4"
-                    />
-                </div>
-                <div className='select'>
-                    <input onChange={() => setGameMode(2)} type="radio" id="memoryGameCheck" name="gameMode" value="memoryGame" checked={gameMode === 2}></input>
+                    </div>
+                    <div className='select'>
+                        <input onChange={() => setGameMode(2)} type="radio" id="memoryGameCheck" name="gameMode" value="memoryGame" checked={gameMode === 2}></input>
 
-                    <label htmlFor="memoryGameCheck"> Memory Game</label>
-                </div>
-                <div className='select'>
-                <input onChange={() => setGameMode(3)} type="radio" id="botGameCheck" name="gameMode" value="botGame" checked={gameMode === 3}></input>
-                    <label htmlFor="botGameCheck"> Bot Game</label>
-                    {gameMode === 3 && (
-                        <div>
-                            <label htmlFor="botDifficultySlider">Bot Difficulty:</label>
-                            <input
-                                id="botDifficultySlider"
-                                type="range"
-                                min="1"
-                                max="10"
-                                value={botDifficulty}
-                                onChange={(e) => setBotDifficulty(parseInt(e.target.value, 10))}
-                                style={{ width: "300px" }} // Optional style to improve visibility
-                            />
-                            <div className="sliderValue">
-                                {botDifficulty} / 10
+                        <label className="hoverLabel" htmlFor="memoryGameCheck"> Memory Game</label>
+                        <div className="hover-text"> Memory game of Connect4 where players can't see the colors of the tokens</div>
+                    </div>
+                    <div className='select'>
+                        <input onChange={() => setGameMode(3)} type="radio" id="botGameCheck" name="gameMode" value="botGame" checked={gameMode === 3}></input>
+                        <label className="hoverLabel" htmlFor="botGameCheck"> Bot Game</label>
+                        <div className="hover-text"> A Connect4 game versus a bot, where You may choose bot difficulty</div>
+                        {gameMode === 3 && (
+                            <div>
+                                <label htmlFor="botDifficultySlider">Bot Difficulty:</label>
+                                <input
+                                    id="botDifficultySlider"
+                                    type="range"
+                                    min="1"
+                                    max="10"
+                                    value={botDifficulty}
+                                    onChange={(e) => setBotDifficulty(parseInt(e.target.value, 10))}
+                                    style={{ width: "300px" }} // Optional style to improve visibility
+                                />
+                                <div className="sliderValue">
+                                    {botDifficulty} / 10
+                                </div>
                             </div>
-                        </div>
-                    )}
-                </div>
-                <div className='select'>
-                    <label htmlFor="heigthInput">Heigth of the board:</label>
-                    <input
-                        id="heigthInput"
-                        type="number"
-                        value={heigth}
-                        onChange={handleHeightChange}
-                        min="3"
-                        max="15"
-                    />
-                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-                </div>
-                <div className='select'>
-                    <label htmlFor="widthInput">Length of the board:</label>
-                    <input
-                        id="widthInput"
-                        type="number"
-                        value={width}
-                        onChange={handleWidthChange}
-                        min="3"
-                        max="15"
-                    />
-                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-                </div>
+                        )}
+                    </div>
+                    <div className='settings'>
+                        <label htmlFor="additionalSettings">Press to show additional settings:</label> <input onChange={() => setShowSettings(true)} type="radio" id="additionalSettings"  checked={showSettings === true}></input>
+                        {showSettings && (
+                            <div>
+                                <div className='select'>
+                                    <label htmlFor="heigthInput">Heigth:</label>
+                                    <input
+                                        className="numberInput"
+                                        id="heigthInput"
+                                        type="number"
+                                        value={heigth}
+                                        onChange={handleHeightChange}
+                                        min="3"
+                                        max="9"
+                                    />
+                                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                                </div>
+                                <div className='select'>
+                                    <label htmlFor="widthInput">Length:</label>
+                                    <input
+                                        className="numberInput"
+                                        id="widthInput"
+                                        type="number"
+                                        value={width}
+                                        onChange={handleWidthChange}
+                                        min="3"
+                                        max="9"
+                                    />
+                                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                                </div>
 
-                   
-           
-                
 
-                <div className='select'>
-                    <label htmlFor="winConditionInput">Number of circles to connect to win:</label>
-                    <input
-                        id="winConditionInput"
-                        type="number"
-                        value={winCondition}
-                        onChange={e => setWinCondition(parseInt(e.target.value, 10))}
-                        min="2"  // Minimum of 3 to make it reasonable
-                    />
-                </div>
-            </form>
-            <div><input id='roomIdInput' className='roomUIelement' type="text" value={inputValue} onChange={handleChange} placeholder='Input Room ID'></input></div>
-            <div><button className='roomUIelement' onClick={handleJoinRoomClick}>Join Game Room</button></div>
-        </div>
-        
-    );
-  }                 
+
+
+
+                                <div className='select'>
+                                    <label htmlFor="winConditionInput">Number of circles:</label>
+                                    <input
+                                        className="numberInput"
+                                        id="winConditionInput"
+                                        type="number"
+                                        value={winCondition}
+                                        onChange={e => setWinCondition(parseInt(e.target.value, 10))}
+                                        min="2"  // Minimum of 3 to make it reasonable
+                                        max="9"
+                                    />
+                                    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                                </div>
+
+                                {gameMode === 1 && (
+                            <div>
+                                <label htmlFor="winConditionInput">Choose number of players:</label>
+                                <input
+                                    className="numberInput"
+                                    id="playerInput"
+                                    type="number"
+                                    value={playerCondition}
+                                    onChange={e => setPlayerCondition(parseInt(e.target.value, 10))}
+                                    min="2"  // Minimum of 3 to make it reasonable
+                                    max="4"
+                                />
+                            </div>
+                        )}
+                            </div>
+                        )}
+                    </div>
+                </form>
+                <div><input className='textInput' id='roomIdInput' type="text" value={inputValue} onChange={handleChange} placeholder='Input Room ID'></input></div>
+                <div><button className='button-6' onClick={handleJoinRoomClick}>Join Game Room</button></div>
+            </div>
+
+        );
+    }
 };
 
 export default GameRoom;
